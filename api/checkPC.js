818586af56e5af0dc471afab396dfb063bd13969
YXS-2000 <1984570040@qq.com>
2022-01-17 00:18:18 +0800

    dynamic import file

--- api/checkPC.js
+++ api/checkPC.js
@@ -1,8 +1,8 @@
-export function checkPC(){
+export async function checkPC(){
+    //通过navigator实现文件动态导入
     if(navigator.userAgent){
-        return ()=>{import App from '../src/App.vue'}
-    }
-    else{
-        return ()=>{import App from '../src/App_mobile.vue'}
+        return ()=>import('../src/App.vue')
+    }else{
+        return ()=>import('../src/App_mobile.vue')
     }
 }
